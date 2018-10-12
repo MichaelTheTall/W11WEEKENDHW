6f606facc30014fa411837bf7b780b922db6210f
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-12 10:26:08 +0100

    init commit

---
+++ src/main/java/Plane.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Plane {
+    private Type type;
+    private Airline airline;
+    private ArrayList<Person> passengers;
+
+}
---
+++ src/main/java/Type.java
@@ -0,0 +1,18 @@
+public enum Type {
+    B747(400),
+    B757(200),
+    B777(300),
+    A330(250),
+    A340(350),
+    A380(600);
+
+    private final int capacity;
+
+    Type(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
