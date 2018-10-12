c2d1460614c860c09239b2cefadc08575723f9e9
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-12 18:32:51 +0100

    added searchforplane

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -10,4 +10,25 @@ public class Airport {
     }
 
 
+    public AirportCode getCode() {
+        return code;
+    }
+
+    public ArrayList getHangar() {
+        return hangar;
+    }
+
+    public void addPlane(Plane plane) {
+        hangar.add(plane);
+    }
+
+    public Plane findPlane(Plane plane) {
+        if (hangar.contains(plane)) {
+            int index = hangar.indexOf(plane);
+            Plane result = hangar.get(index);
+            return result;
+        } else {
+            return null;
+        }
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,9 +1,13 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class AirportTest {
 
     Airport airport;
     Plane plane;
+    Plane findme;
     Person person;
     Flight flight;
 
@@ -13,5 +17,42 @@ public class AirportTest {
         plane = new Plane(Type.B747, Airline.BA);
         person = new Person("Testman");
         flight = new Flight(plane, 888, AirportCode.HKG);
+        findme = new Plane(Type.A380,Airline.EJ);
+    }
+
+    @Test
+    public void getAirportCode(){
+        assertEquals(AirportCode.EDI, airport.getCode());
+    }
+
+    @Test
+    public void getHangar(){
+        airport.addPlane(plane);
+        assertEquals(1, airport.getHangar().size());
+    }
+
+    @Test
+    public void checkHangarForPlane(){
+        airport.addPlane(plane);
+        airport.addPlane(plane);
+        airport.addPlane(findme);
+        airport.addPlane(plane);
+        assertEquals(4, airport.getHangar().size());
+        assertEquals(findme, airport.findPlane(findme));
+    }
+
+    @Test
+    public void checkHangarForInvalidPlane(){
+    airport.addPlane(plane);
+    airport.addPlane(plane);
+    airport.addPlane(plane);
+    airport.addPlane(plane);
+    assertEquals(4, airport.getHangar().size());
+    assertEquals(null, airport.findPlane(findme));
+    }
+
+    @Test
+    public void bookFlight(){
+        
     }
 }
