e3c002203b6745b67f3762ac247352f2a2cf1c8d
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-12 10:46:10 +0100

    TDD plane continues

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -7,8 +7,32 @@ public class Plane {
 
     public Plane(Type type, Airline airline){
         this.type = type;
-        this. airline = airline;
+        this.airline = airline;
         passengers = new ArrayList<>();
     }
 
+    public Type getType() {
+        return type;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public int maxCapacity() {
+        return type.getCapacity();
+    }
+
+    public int boarded() {
+        return passengers.size();
+    }
+
+    public void addPassenger(Person person) {
+        passengers.add(person);
+    }
+
+    public int space() {
+        int result = maxCapacity() - boarded();
+        return result;
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,12 +1,53 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PlaneTest {
 
     Plane plane;
+    Person person;
 
     @Before
     public void before(){
         plane = new Plane(Type.B747, Airline.BA);
+        person = new Person("Testman");
     }
 
+    @Test
+    public void getType(){
+        assertEquals(Type.B747, plane.getType());
+    }
+
+    @Test
+    public void getAirline(){
+        assertEquals(Airline.BA, plane.getAirline());
+    }
+
+    @Test
+    public void getCapacity(){
+        assertEquals(400, plane.maxCapacity());
+    }
+
+    @Test
+    public void getBoardedEmpty(){
+        assertEquals(0, plane.boarded());
+    }
+
+    @Test
+    public void addPassenger(){
+        plane.addPassenger(person);
+        assertEquals(1, plane.boarded());
+    }
+
+    @Test
+    public void getSpaceLeft(){
+        plane.addPassenger(person);
+        plane.addPassenger(person);
+        plane.addPassenger(person);
+        plane.addPassenger(person);
+        plane.addPassenger(person);
+        assertEquals(5, plane.boarded());
+        assertEquals(395, plane.space());
+    }
 }
